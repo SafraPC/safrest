780a07f91aa3be715692a158df4aed291b7855e2
Felipe Carradori <56209331+Carradori@users.noreply.github.com>
2021-04-27 09:07:21 -0300

    fix: carousel width and height responsive

--- src/pages/Home/carousel.js
+++ src/pages/Home/carousel.js
@@ -1,48 +1,39 @@
-import React from 'react';
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
-import ambient from '../../assets/ambient.jpg'
-import beauty from '../../assets/beauty.jpg'
-import peopleUsing from '../../assets/peopleUsing.jpg'
-import { Carousel } from 'react-bootstrap';
+import ambient from "../../assets/ambient.jpg";
+import beauty from "../../assets/beauty.jpg";
+import peopleUsing from "../../assets/peopleUsing.jpg";
+import { Carousel } from "react-bootstrap";
+
 function HomeCarousel() {
-  return (
-<Carousel className="navbar myCarrousel">
-  <Carousel.Item interval={2500} >
-    <img
-      className="d-block w-100 myImage"
-      src={ambient}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={2500}  >
-    <img
-      className="d-block w-100 myImage"
-      src={beauty}
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={2500}>
-    <img
-      className="d-block w-100 myImage "
-      src={peopleUsing}
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-  );
+	return (
+		<Carousel style={{ height: "650px" }}>
+			<Carousel.Item interval={2500}>
+				<img className="d-block" src={ambient} alt="First slide" />
+				<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item interval={2500}>
+				<img className="d-block" src={beauty} alt="Second slide" />
+				<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item interval={2500}>
+				<img className="d-block " src={peopleUsing} alt="Third slide" />
+				<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>
+						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+					</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
+	);
 }
 
 export default HomeCarousel;
--- src/styles/default.js
+++ src/styles/default.js
@@ -1,21 +1,22 @@
 import styled, { css } from "styled-components";
 
 export const Page = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  background-color: #edf4f5;
-`
+	width: 100%;
+	min-height: 100vh;
+	background-color: #edf4f5;
+`;
 export const view = css`
-  width: 100%;
-  height: auto;
-  min-height: 90vh;
-  display: flex;
-  margin: 0 auto;
-  flex-direction: column;
-  background-color: #fff;
-  border-radius: 0.1rem;
+	width: 100%;
+	max-width: 1440px;
+	height: auto;
+	min-height: 90vh;
+	display: flex;
+	margin: 0 auto;
+	flex-direction: column;
+	background-color: #fff;
+	border-radius: 0.1rem;
 `;
 
 export const ScreenView = styled.div`
-${view};
-`
+	${view};
+`;
