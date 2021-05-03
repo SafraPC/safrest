becdc66e7ab8de8e1754cd1bc2221ee55749e11a
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-05-03 13:01:03 -0300

    fix validation

--- src/pages/WorkWithUs/index.js
+++ src/pages/WorkWithUs/index.js
@@ -14,8 +14,8 @@ import { customStyles } from "../../styles/selectStyles";
 import { cities, states } from "../../services/locations";
 import { useState } from "react";
 import { useEffect } from "react";
-import * as yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { workSchema } from "../../validations/workTogether";
 
 function WorkWithUs() {
   const [cidades, setCidades] = useState([]);
@@ -31,15 +31,6 @@ function WorkWithUs() {
     cities(selectedState.value).then(setCidades).catch(console.error);
   }, [selectedState]);
 
-  const workSchema2 = yup.object().shape({
-    name: yup
-      .string("Need to be an Text!")
-      .min(4, "Min text is 4!")
-      .max(30, "Max Text is 30!")
-      .required(),
-    age: yup.string().required().max(3).min(1),
-    email: yup.string().email().required(),
-  });
 
   const handleFormSubmit = (values) => {
     console.log(values);
@@ -65,21 +56,21 @@ function WorkWithUs() {
               age: "",
               email: "",
             }}
-            validationSchema={workSchema2}
+            validationSchema={workSchema}
             onSubmit={handleFormSubmit}
           >
+            {({errors,touched})=>(
               <Form>
                 <span>TEXT EXAMPLE</span>
                 <Field type="text" placeholder="Nome" name="name" />
-                <ErrorMessage component="p" name="name" />
-
+                {errors.name && touched.name ? (
+                <span>{errors.name}</span>
+                ) : null}
                 <span>TEXT EXAMPLE</span>
                 <Field type="text" placeholder="Idade" name="age" />
-                <ErrorMessage component="p" name="age" />
 
                 <span>TEXT EXAMPLE</span>
                 <Field type="text" placeholder="Email" name="email" />
-                <ErrorMessage component="p" name="email" />
 
                 <span>Estado</span>
                 <MySelect
@@ -99,11 +90,13 @@ function WorkWithUs() {
                   noOptionsMessage={() => "SEM NADA"}
                   options={cidades}
                 />
-                {/* <span>TEXT EXAMPLE</span>
-                <Field type="text" placeholder="Curriculo" /> */}
+                <span>TEXT EXAMPLE</span>
+                <Field type="text" placeholder="Curriculo" name="curriculo" />
 
                 <button type="submit">Enviar</button>
               </Form>
+            )}
+              
           </Formik>
         </TForm>
       </ScreenView>
